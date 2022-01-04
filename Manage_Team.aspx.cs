d9637f2735e14de9def0a879e634812caf09806d
Shivam Gupta <66762938+Shivam755@users.noreply.github.com>
2022-01-04 15:20:11 +0530

    Firestore connection(In progress)

--- Manage_Team.aspx.cs
+++ Manage_Team.aspx.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Google.Cloud.Firestore;
+using System.Threading.Tasks;
 
 namespace Khushiyaan
 {
     public partial class Manage_Team : System.Web.UI.Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
+            String path = AppDomain.CurrentDomain.BaseDirectory + @"khushiyaan-48310-firebase-adminsdk-n6h2h-92ec6e587c.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+            FirestoreDb db = FirestoreDb.Create("khushiyaan-48310");
+            
 
         }
     }
