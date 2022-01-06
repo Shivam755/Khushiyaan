2cf35bf28dffd452f14f6c023896d3556e88348c
hrshl13 <harshal26092002@gmail.com>
2022-01-07 00:56:18 +0530

    VieewMessages Updated

--- Projects.aspx.designer.cs
+++ Projects.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace Khushiyaan
 
     public partial class Projects
     {
-
-        /// <summary>
-        /// parent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl parent;
     }
 }
